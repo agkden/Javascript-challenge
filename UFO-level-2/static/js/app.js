5f72142ea91fadb9d8726cbae198156b0264de72
agkden <agkden@yahoo.com>
2021-03-24 22:44:24 -0600

    update file
    added script for multiple filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -13,7 +13,7 @@ function buildTable(data) {
   data.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
-      row.append('td').text(value);
+      row.append("td").text(value);
     });
   });
 }
@@ -22,14 +22,53 @@ function buildTable(data) {
 // Select the button
 var button = d3.select("#filter-btn");
 
-// Select the form
-var form = d3.select("#form");
-
 // Create event handlers 
-button.on("click", filterSearch);
-form.on("submit", filterSearch);
+button.on("click", filterMultiSearch);
 
+// Create the Filter function to run for click event
+function filterMultiSearch() {
 
+  // Clear table before rendering again
+  tbody.html("");
 
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
 
+  var filteredData = tableData
+
+  // Select the input element and get the value property
+  // --> Date
+  var inputDate = d3.select("#datetime").property("value");
+  if (inputDate) {
+    filteredData = filteredData.filter(date => date.datetime === inputDate);
+  };
+
+    // --> City
+  var inputCity = d3.select("#city").property("value");
+  if (inputCity) {
+    filteredData = filteredData.filter(xcity => xcity.city === inputCity);
+  };
+
+    // --> State
+  var inputState = d3.select("#state").property("value");
+  if (inputState) {
+    filteredData = filteredData.filter(xstate => xstate.state === inputState);
+  };
+
+    // --> Country
+  var inputCountry = d3.select("#country").property("value");
+  if (inputCountry) {
+    filteredData = filteredData.filter(xcountry => xcountry.country === inputCountry);
+  };
+
+    // --> Shape
+  var inputShape = d3.select("#shape").property("value");
+  if (inputShape) {
+    filteredData = filteredData.filter(xshape => xshape.shape === inputShape);
+  }; 
+
+  // render table with filtered data
+  buildTable(filteredData);
+
+}
 
